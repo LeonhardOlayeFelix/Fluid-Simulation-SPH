55ed0fb3c8a8baec64ef15316dce8fde24bdb856
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2026-09-13 02:09:11 +0100

    Get ImGui window working

--- FluidSimulation/src/FluidSimulation.cpp
+++ FluidSimulation/src/FluidSimulation.cpp
@@ -1,21 +1,24 @@
-#include <glad.h>
-#include <glfw3.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include "Log.h"
-
+#include "vendor/imgui/imgui.h"
+#include "vendor/imgui/imgui_impl_glfw.h"
+#include "vendor/imgui/imgui_impl_opengl3.h"
 
 GLFWwindow* initGLFW();
 void initGLAD();
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void initImGui(GLFWwindow* window);
+void initLogger();
 
 int main()
 {
     GLFWwindow* window = initGLFW();
     initGLAD();
-
-    Log::init();
-    SPHCRITICAL("Hello");
+    initImGui(window);
+    initLogger();
 
     glViewport(0, 0, 800, 600);
 
@@ -23,11 +26,43 @@ int main()
     {
         processInput(window);
 
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("hello");
+        ImGui::End();
+
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
+}
+
+void initLogger() {
+    Log::init();
+}
+void initImGui(GLFWwindow* window) {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
 }
 
 void initGLAD() {
